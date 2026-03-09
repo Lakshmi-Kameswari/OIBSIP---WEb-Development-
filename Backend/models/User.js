68084cc3823a1e006740f3d0db3ea6e537ce7f29
Lakshmi-Kameswari <238x1a4264@khitguntur.ac.in>
2026-03-09 06:51:30 +0530

    Add User model with schema and default values

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,1 +1,17 @@
+const mongoose = require("mongoose")
 
+const UserSchema = new mongoose.Schema({
+    name:String,
+    email:String,
+    password:String,
+    role:{
+        type:String,
+        default:"user"
+    },
+    verified:{
+        type:Boolean,
+        default:false
+    }
+})
+
+module.exports = mongoose.model("User",UserSchema)
