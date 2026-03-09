1bbddd13cfb5b6eb3e69bed2f60ede36a959610a
Lakshmi-Kameswari <238x1a4264@khitguntur.ac.in>
2026-03-09 06:53:55 +0530

    Add Login component to Pizza Delivery App

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,1 +1,19 @@
+import React from "react"
+import "./styles.css"
+import Login from "./components/Login"
 
+function App(){
+
+return(
+<div className="app">
+
+<h1>🍕 Pizza Delivery App</h1>
+
+<Login/>
+
+</div>
+)
+
+}
+
+export default App
